7d429f441d9dc67772ae7b041abd84c0d9405edd
Alain Rouleau <git.commit@alainrouleau.com>
2021-01-11 03:39:47 -0500

    Rename variable

--- src/utilities/reader.py
+++ src/utilities/reader.py
@@ -4,7 +4,7 @@ Data readers for various formats """
 import json
 
 
-def read_json(json_file: str) -> dict:
-    with open(json_file, "r", encoding="utf-8") as read_file:
+def read_json(filename: str) -> dict:
+    with open(filename, "r", encoding="utf-8") as read_file:
         data = json.load(read_file, encoding="UTF-8")
     return data
