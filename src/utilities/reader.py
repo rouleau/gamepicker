eb0881351b7eeb8f19b89dd4183742dc56fa6952
Alain Rouleau <github@alainrouleau.com>
2021-01-12 04:49:16 -0500

    Update reader module

--- src/utilities/reader.py
+++ src/utilities/reader.py
@@ -1,10 +1,10 @@
 """
-Data readers for various formats """
+File readers for various data formats """
 
 import json
 
 
 def read_json(filename: str) -> dict:
     with open(filename, "r", encoding="utf-8") as read_file:
-        data = json.load(read_file, encoding="UTF-8")
+        data = json.load(read_file)
     return data
