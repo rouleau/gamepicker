340932ac36fff7a5ba3dc6f3bc40fbc3d1685a23
Alain Rouleau <github@alainrouleau.com>
2021-01-30 14:48:08 -0500

    Develop GamePool class

--- src/pool.py
+++ src/pool.py
@@ -1,15 +1,15 @@
 """
-Pool module """
+Game Pool module """
 
 from game import InstantGame, LotteryGame
 
 
 class GamePool:
-    """ Create a pool object for game objects """
+    """ Create a game pool object for game objects """
 
     def __init__(self) -> None:
         """
-        Initialize the pool object
+        Initialize the game pool object
 
         >>> game_pool = GamePool()
         >>> game_pool
@@ -21,9 +21,82 @@ class GamePool:
 
         self.games = []
 
+    def __str__(self) -> str:
+        """
+        Return a string representation of the game pool object which
+        includes all games and bets by overriding the __str__ method.
+
+        >>> lotteries = [
+        ...     {
+        ...         "name": "Astro",
+        ...         "is_active": True,
+        ...         "bets": [
+        ...             {
+        ...                 "theoretical_payoff_rate": 0.4372,
+        ...                 "cost": [1, 2, 5],
+        ...                 "daily_draw": True,
+        ...                 "draw_days": []
+        ...             }
+        ...         ]
+        ...     },
+        ...     {
+        ...         "name": "Lotto Max",
+        ...         "is_active": True,
+        ...         "bets": [
+        ...             {
+        ...                 "theoretical_payoff_rate": 0.48,
+        ...                 "cost": [5],
+        ...                 "daily_draw": False,
+        ...                 "draw_days": ["Tuesday", "Friday"]
+        ...             },
+        ...             {
+        ...                 "theoretical_payoff_rate": 0.495,
+        ...                 "cost": [10],
+        ...                 "daily_draw": False,
+        ...                 "draw_days": ["Tuesday", "Friday"]
+        ...             }
+        ...         ]
+        ...     }
+        ... ]
+        >>> game_pool = GamePool()
+        >>> game_pool.add_games(lotteries, "Lottery")
+        >>> print(game_pool)
+        Game 1
+        Name: Astro
+        Active: True
+        Bet 1
+        Theoretical Payoff Rate: 0.4372
+        Cost: [1, 2, 5]
+        Daily Draw: True
+        Draw Days: []
+        <BLANKLINE>
+        Game 2
+        Name: Lotto Max
+        Active: True
+        Bet 1
+        Theoretical Payoff Rate: 0.48
+        Cost: [5]
+        Daily Draw: False
+        Draw Days: ['Tuesday', 'Friday']
+        Bet 2
+        Theoretical Payoff Rate: 0.495
+        Cost: [10]
+        Daily Draw: False
+        Draw Days: ['Tuesday', 'Friday']
+
+        """
+
+        str_repr = ""
+
+        # Create a string from all games
+        for idx, game in enumerate(self.games, start=1):
+            str_repr = str_repr + f"Game {idx}\n{str(game)}"
+
+        return str_repr
+
     def add_games(self, games: list[dict], category: str) -> None:
         """
-        Create and add game objects to the pool object
+        Create and add game objects to the game pool object
 
         >>> lotteries = [
         ...     {
