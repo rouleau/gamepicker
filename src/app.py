44ca76a972b371561ee77cff977fd949d388f30c
Alain Rouleau <github@alainrouleau.com>
2021-01-31 17:13:23 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ __version__ = "0.0.0"
 __author__ = "Alain Rouleau"
 __copyright__ = "Copyright © 2021 Alain Rouleau. All rights reserved."
 
+from event import Schedule
+from pick import Picker
 from pool import GamePool
 from utilities.reader import read_json
 
@@ -22,6 +24,20 @@ my_game_pool.add_games(lotteries, "Lottery")
 # Display all games and bets in the game pool
 print(my_game_pool)
 
-# Pick a random payoff rate based on weight
+# Display all theoretical payoff rates
 theoretical_payoff_rates = my_game_pool.payoff_rates()
 print(theoretical_payoff_rates)
+
+# Pick a random payoff rate based on weight
+picker = Picker()
+payoff_rate = picker.payoff_rate(theoretical_payoff_rates)
+print(f"Payoff Rate: {payoff_rate}")
+
+# Create a schedule
+schedule = Schedule("2021-02-01", "2021-04-30")
+print()
+print(schedule.dates)
+
+random_dates = schedule.random_dates(30)
+print()
+print(random_dates)
