721f9b8aba449b799b6d2e0306678316a84d33b6
Alain Rouleau <github@alainrouleau.com>
2021-02-01 17:04:04 -0500

    Remove payoff_rate() from Picker

--- src/pick.py
+++ src/pick.py
@@ -17,27 +17,6 @@ class Picker:
 
         """
 
-    def payoff_rate(self, payoff_rates: list[float]) -> float:
-        """
-        Return a single payoff rate based on randomness and weight from a list
-        of unique theoretical payoff rates from all bets
-
-        >>> payoff_rates = [
-        ...     0.4372, 0.45, 0.4555, 0.47, 0.48, 0.49, 0.499,
-        ...     0.5, 0.5017, 0.5462, 0.578, 0.5818, 0.6, 0.6259,
-        ...     0.6328, 0.64, 0.6622, 0.8479, 0.85, 0.8705, 0.93,
-        ... ]
-        >>> picker = Picker()
-        >>> payoff_rate = picker.payoff_rate(payoff_rates)
-        >>> payoff_rate # doctest: +SKIP
-        0.8479
-
-        """
-
-        payoff_rate = random.choices(payoff_rates, weights=payoff_rates, k=1)
-
-        return payoff_rate[0]
-
 
 def test():
     """ Test docstrings using doctest """
