0d45b9eb35a86699d3e62ebbf8e5d3e5ba153eba
Alain Rouleau <github@alainrouleau.com>
2021-02-03 15:08:48 -0500

    Reorganize gamepicker.py as main entry point for project

--- src/gamepicker.py
+++ src/gamepicker.py
@@ -1,36 +1,11 @@
 """
-Game Picker module """
+PROJECT: GAMEPICKER
 
+- Main entry point
+- Run as py -m gamepicker
 
-def picks():
-    """
-    Display a list of bets to purchase
+"""
 
-    >>> picks()
-    These are your picks:
-    * Buy ... for $...
-    <BLANKLINE>
-    Good Luck!
-    """
-    bets = [("Astro", "$1.00"), ("Lotto Max", "$5.00")]
+import app
 
-    print("These are your picks:")
-
-    for bet in bets:
-        print(f"* Buy a {bet[0]} for {bet[1]}")
-
-    print()
-    print("Good Luck!")
-
-
-def main():
-    """
-    Main function
-    """
-    import doctest
-
-    doctest.testmod(verbose=True, optionflags=doctest.ELLIPSIS)
-
-
-if __name__ == "__main__":
-    main()
+app.main()
