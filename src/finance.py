a70b09d9edae340b5d4a8cd96de1907d4da18783
Alain Rouleau <github@alainrouleau.com>
2021-02-03 15:15:14 -0500

    Develop Budget class

--- src/finance.py
+++ src/finance.py
@@ -1,19 +1,43 @@
 """
 Finance module """
 
+from datetime import datetime
+
 
 class Budget:
-    """ Create a budget for picking bets and buying tickets """
+    """ Create a budget for placing bets """
 
-    def __init__(self) -> None:
+    def __init__(self, balance: float = 0.00, period: str = "Day") -> None:
         """
         Initialize the budget object
 
         >>> budget = Budget()
         >>> budget
         <__main__.Budget object at 0x...>
+        >>> budget.balance
+        0.0
+        >>> budget.period
+        'Day'
 
         """
+
+        self.balance = balance
+        self.period = period
+
+    def add_funds(self, amount: float) -> None:
+        """
+        Add funds to budget balance
+
+        >>> budget = Budget()
+        >>> budget.balance
+        0.0
+        >>> budget.add_funds(5.75)
+        >>> budget.balance
+        5.75
+
+        """
+
+        self.balance = self.balance + amount
 
 
 def test():
