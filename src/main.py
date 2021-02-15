44b1edf285d6f4fc9996bb1ca0b1ca2823fd3fc2
Alain Rouleau <github@alainrouleau.com>
2021-02-15 14:05:25 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,36 +12,37 @@ def app():
     lotteries = read_json("../data/games/lotteries.json")["games"]
 
     # Create and add game objects to a game pool object
-    my_game_pool = GamePool()
-    my_game_pool.add_games(instants, "Instant")
-    my_game_pool.add_games(lotteries, "Lottery")
+    game_pool = GamePool()
+    game_pool.add_games(instants, "Instant")
+    game_pool.add_games(lotteries, "Lottery")
 
-    # Display all games and bets in the game pool
-    print(my_game_pool)
+    # Print all games and bets in the game pool
+    print(game_pool)
 
-    # Display all theoretical payoff rates
-    theoretical_payoff_rates = my_game_pool.payoff_rates()
+    # Print all theoretical payoff rates
+    theoretical_payoff_rates = game_pool.payoff_rates()
     print(theoretical_payoff_rates)
 
-    # Display a payoff rate based on randomness and weight from the set of
+    # Print a payoff rate based on randomness and weight from the set of
     # theoretical payoff rates from all bets
-    payoff_rate = my_game_pool.payoff_rate()
+    payoff_rate = game_pool.payoff_rate()
     print(f"Payoff Rate: {payoff_rate}")
 
     # Create a schedule
-    schedule = Schedule("2021-02-01", "2021-04-30")
+    schedule = Schedule("2021-02-15", "2021-04-30")
     print()
     print(schedule.dates)
 
-    # Create and display a list of 30 random date objects
+    # Create and print a list of 30 random date objects
     random_dates = schedule.random_dates(30)
     print()
     print(random_dates)
 
-    # Display the list of 30 random date objects as strings
+    # Create and print a list of 30 random date objects as strings
+    random_dates = schedule.random_dates_to_str(30)
     print()
-    for num, item in enumerate(random_dates, start=1):
-        print(num, item.isoformat(), item.strftime("%A, %B %d, %Y"))
+    for random_date in random_dates:
+        print(random_date)
 
     # Start Game Picker
     print("\nWelcome to the Espacejeux Game Picker!\n")
