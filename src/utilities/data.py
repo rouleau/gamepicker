6d7b11f4f7d5cdd8883a97645bd7d38a84f3ce86
Alain Rouleau <github@alainrouleau.com>
2021-02-16 17:52:58 -0500

    Rename docstring and read_json variable

--- src/utilities/data.py
+++ src/utilities/data.py
@@ -1,10 +1,10 @@
 """
-Data module for various formats """
+Data utilities for various formats """
 
 import json
 
 
 def read_json(filename: str) -> dict:
-    with open(filename, "r", encoding="utf-8") as read_file:
-        data = json.load(read_file)
+    with open(filename, "r", encoding="utf-8") as file:
+        data = json.load(file)
     return data
