11f182f04b6a0ef26a956c594e8640d74497d967
Alain Rouleau <github@alainrouleau.com>
2021-02-18 07:03:25 -0500

    Add menus for interface

--- src/constants/menus.py
+++ src/constants/menus.py
@@ -2,21 +2,36 @@
 Menus
 
 """
-
 MAIN = {
     "header": "Welcome to the Espacejeux Game Picker!",
-    1: "Create a Schedule",
-    2: "Print Games",
-    3: "Option 3",
-    4: "Option 4",
-    9: "Option 9",
+    1: "Games",
+    2: "Budget",
+    3: "Schedule",
+    4: "Exit",
+}
+
+GAMES = {
+    "header": "GAMES",
+    1: "Print games",
+    2: "Exit",
+}
+
+BUDGET = {
+    "header": "BUDGET",
+    1: "Create budget",
+    2: "Exit",
 }
 
 SCHEDULE = {
-    "header": "Schedule",
-    1: "Print random date objects",
-    2: "Print random date objects as strings",
-    3: "Write random dates to CSV file",
-    4: "Option 4",
-    9: "Option 9",
+    "header": "SCHEDULE",
+    1: "Create schedule",
+    2: "Exit",
+}
+
+SCHEDULE_SUB_MENU = {
+    "header": "SCHEDULE · OPTIONS",
+    1: "Print random dates as objects",
+    2: "Print random dates as strings",
+    3: "Write random dates as strings to CSV file",
+    4: "Exit",
 }
