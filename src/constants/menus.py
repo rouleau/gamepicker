123b3608d31d457d542437312ed3337faeebce2c
Alain Rouleau <github@alainrouleau.com>
2021-02-18 00:00:58 -0500

    Add Menus for (CLI) command line interface

---
+++ src/constants/menus.py
@@ -0,0 +1,22 @@
+"""
+Menus
+
+"""
+
+MAIN = {
+    "header": "Welcome to the Espacejeux Game Picker!",
+    1: "Create a Schedule",
+    2: "Print Games",
+    3: "Option 3",
+    4: "Option 4",
+    9: "Option 9",
+}
+
+SCHEDULE = {
+    "header": "Schedule",
+    1: "Print random date objects",
+    2: "Print random date objects as strings",
+    3: "Write random dates to CSV file",
+    4: "Option 4",
+    9: "Option 9",
+}
