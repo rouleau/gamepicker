ccf1c6cfeaff84430867a2a6937d4b2eb52be433
Alain Rouleau <github@alainrouleau.com>
2021-02-18 06:58:30 -0500

    Develop app.py

--- src/app.py
+++ src/app.py
@@ -9,67 +9,133 @@ def main():
     Main function for the gamepicker application
 
     """
-
-    from constants.menus import SCHEDULE
-    from schedule import Schedule
-    from interface import menu
+    from constants.menus import BUDGET, GAMES, SCHEDULE, SCHEDULE_SUB_MENU
+    from interface import menu, menu_exit, menu_invalid
     from pool import GamePool
+    from schedule import Schedule
     from utilities.data import read_json
 
-    # Read games from JSON files
-    instants = read_json("../data/games/instants.json")["games"]
-    lotteries = read_json("../data/games/lotteries.json")["games"]
+    # START GAME PICKER
+    print()
 
-    # Create and add game objects to a game pool object
-    game_pool = GamePool()
-    game_pool.add_games(instants, "Instant")
-    game_pool.add_games(lotteries, "Lottery")
+    # Print MAIN menu and return selection
+    selection = menu()
 
-    # Print all games and bets in the game pool
-    print(game_pool)
+    if selection == 1:  # GAMES
 
-    # Print all theoretical payoff rates
-    theoretical_payoff_rates = game_pool.payoff_rates()
-    print(theoretical_payoff_rates)
+        # Read games from JSON files
+        instants = read_json("../data/games/instants.json")["games"]
+        lotteries = read_json("../data/games/lotteries.json")["games"]
 
-    # Print a payoff rate based on randomness and weight from the set of
-    # theoretical payoff rates from all bets
-    payoff_rate = game_pool.payoff_rate()
-    print(f"Payoff Rate: {payoff_rate}")
+        # Create and add game objects to a game pool object
+        game_pool = GamePool()
+        game_pool.add_games(instants, "Instant")
+        game_pool.add_games(lotteries, "Lottery")
 
-    # Start Game Picker
-    # Print main menu and return selection
-    menu_selection = menu()
+        # Print GAMES menu and return selection
+        selection = menu(GAMES)
 
-    if menu_selection == 1:
-        print("Create a Schedule")
+        if selection == 1:  # Print Games
 
-        # Create a schedule
-        schedule = Schedule("2021-02-15", "2021-04-30")
-        print()
-        print(schedule.dates)
+            # Print all games and bets in the game pool
+            print(game_pool)
 
-        # Create and print a list of 30 random date objects
-        random_dates = schedule.random_dates(30)
-        print()
-        print(random_dates)
+            # Print all theoretical payoff rates
+            theoretical_payoff_rates = game_pool.payoff_rates()
+            print("PAYOFF RATES")
+            print("------------")
+            print()
+            print(theoretical_payoff_rates)
 
-        # Create and print a list of 30 random date objects as strings
-        random_dates = schedule.random_dates_to_str(30)
-        print()
-        for random_date in random_dates:
-            print(random_date)
+            # Print a payoff rate based on randomness and weight from the set of
+            # theoretical payoff rates from all bets
+            payoff_rate = game_pool.payoff_rate()
+            print()
+            print(f"Payoff Rate: {payoff_rate} (randomly selected based on weight)")
 
-        # Create and write a list of 30 random dates to a CSV file in the
-        # datetime sub-folder of the data folder
-        print()
-        print("Writing to CSV file...")
-        schedule.random_dates_to_csv(30)
-        print("Done!")
+        elif selection == 2:  # Print exit message
+            menu_exit()
 
-    else:
-        print(f"Option {menu_selection}")
-        print("Goodbye!")
+        else:  # Print invalid option message
+            menu_invalid(selection)
+
+    elif selection == 2:  # BUDGET
+
+        # Print BUDGET menu and return selection
+        selection = menu(BUDGET)
+
+        if selection == 1:  # Create budget
+
+            # TODO Create budget
+            print("Creating budget...")
+
+        elif selection == 2:  # Print exit message
+            menu_exit()
+
+        else:  # Print invalid option message
+            menu_invalid(selection)
+
+    elif selection == 3:  # SCHEDULE
+
+        # Print SCHEDULE menu and return selection
+        selection = menu(SCHEDULE)
+
+        if selection == 1:  # Create Schedule
+
+            # Input start and end dates for schedule
+            start_date = input("Start Date [2021-01-01]: ") or "2021-01-01"
+            end_date = input("End Date   [2021-03-31]: ") or "2021-03-31"
+
+            # Create a schedule
+            schedule = Schedule(start_date, end_date)
+            total_days = len(schedule.dates)
+
+            print()
+            print(f"Schedule created from {start_date} to {end_date}")
+            print(f"Total Days: {total_days}")
+            print()
+
+            # Print SCHEDULE sub-menu and return selection
+            sub_selection = menu(SCHEDULE_SUB_MENU)
+
+            if sub_selection == 1:  # Print random dates as objects
+
+                # Create and print a list of 30 random date objects
+                random_dates = schedule.random_dates(30)
+                print(random_dates)
+
+            elif sub_selection == 2:  # Print random dates as strings
+
+                # Create and print a list of 30 random date objects as strings
+                random_dates = schedule.random_dates_to_str(30)
+                for random_date in random_dates:
+                    print(random_date)
+
+            elif sub_selection == 3:  # Write random dates as strings to CSV file
+
+                # Create and write a list of 30 random dates to a CSV file in the
+                # datetime sub-folder of the data folder
+                print("Writing random dates as strings to CSV file...")
+                schedule.random_dates_to_csv(30)
+                print("Done!")
+
+            elif sub_selection == 4:  # Print exit message
+                menu_exit()
+
+            else:  # Print invalid option message
+                menu_invalid(sub_selection)
+
+        elif selection == 2:  # Print exit message
+            menu_exit()
+
+        else:  # Print invalid option message
+            menu_invalid(selection)
+
+    elif selection == 4:  # Print exit message
+        menu_exit()
+
+    else:  # Print invalid option message
+        menu_invalid(selection)
 
 
 if __name__ == "__main__":
