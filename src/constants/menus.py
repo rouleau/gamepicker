da61009070d98f1e9bec298a7ac7afee0aad93c3
Alain Rouleau <github@alainrouleau.com>
2021-02-20 23:59:40 -0500

    Remove "Exit" from menus

--- src/constants/menus.py
+++ src/constants/menus.py
@@ -7,25 +7,21 @@ MAIN = {
     1: "Games",
     2: "Budget",
     3: "Schedule",
-    4: "Exit",
 }
 
-GAMES = {
-    "header": "GAMES",
+GAME = {
+    "header": "GAME",
     1: "Print games",
-    2: "Exit",
 }
 
 BUDGET = {
     "header": "BUDGET",
     1: "Create budget",
-    2: "Exit",
 }
 
 SCHEDULE = {
     "header": "SCHEDULE",
     1: "Create schedule",
-    2: "Exit",
 }
 
 SCHEDULE_SUB_MENU = {
@@ -33,5 +29,4 @@ SCHEDULE_SUB_MENU = {
     1: "Print random dates as objects",
     2: "Print random dates as strings",
     3: "Write random dates as strings to CSV file",
-    4: "Exit",
 }
