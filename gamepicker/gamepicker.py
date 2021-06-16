a36812477140b6d3c2269a05f89d98b4aca845db
Alain Rouleau <github@alainrouleau.com>
2021-06-16 05:21:30 -0400

    Reconfigure gamepicker.py

--- gamepicker/gamepicker.py
+++ gamepicker/gamepicker.py
@@ -3,15 +3,13 @@ PROJECT: GAMEPICKER
 
 An advanced toolkit for selecting Loto-Québec games
 
-- Main entry point
-- Run as python -m gamepicker
-
 """
 
 __version__ = "0.0.0"
 __author__ = "Alain Rouleau"
 __copyright__ = "Copyright © 2021 Alain Rouleau. All rights reserved."
 
-import app
+import random
 
-app.main()
+from datetime import datetime
+from utilities.data import read_json
