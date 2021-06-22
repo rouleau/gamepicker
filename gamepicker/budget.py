e591be42955c0febf6ee569c9efa6b6844807f42
Alain Rouleau <github@alainrouleau.com>
2021-06-22 18:06:26 -0400

    Add decrease method to Budget class

--- gamepicker/budget.py
+++ gamepicker/budget.py
@@ -4,7 +4,7 @@
 class Budget:
     """ Create a budget for placing bets """
 
-    def __init__(self, balance: float = 0.00, period: str = "Day") -> None:
+    def __init__(self, balance: float = 0.0, period: str = "Day") -> None:
         """
         Initialize the budget object
 
@@ -20,14 +20,31 @@ class Budget:
         self.balance = balance
         self.period = period
 
-    def add_funds(self, amount: float) -> None:
+    def decrease(self, amount: float) -> None:
         """
-        Add funds to budget balance
+        Decrease budget balance
+
+        >>> budget = Budget(10.25)
+        >>> budget.balance
+        10.25
+        >>> budget.decrease(5.75)
+        >>> budget.balance
+        4.5
+        >>> budget.decrease(5.75)
+        >>> budget.balance
+        -1.25
+
+        """
+        self.balance = self.balance - amount
+
+    def increase(self, amount: float) -> None:
+        """
+        Increase budget balance
 
         >>> budget = Budget()
         >>> budget.balance
         0.0
-        >>> budget.add_funds(5.75)
+        >>> budget.increase(5.75)
         >>> budget.balance
         5.75
 
